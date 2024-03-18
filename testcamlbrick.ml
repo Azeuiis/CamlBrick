318d851a249accef1c11f3504ea7bd14248a5972
Kamardine <76865759+Azeuiis@users.noreply.github.com>
2024-03-18 16:46:48 +0100

    Add files via upload

---
+++ testcamlbrick.ml
@@ -0,0 +1,9 @@
+(**
+    Ce fichier contient les fonctions de testes pour les fonctions de notre projet de camlbrick 
+    @author Kamardine
+    @author Antoine
+    @author Nicolas
+    @author Olivier
+
+*)
+
